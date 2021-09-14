728d23c039f07230f0c6814c26eada5235de75e4
jiajing <jiajing@zhiweidata.com>
2021-09-14 11:28:22 +0800

    新建rollup.config.js文件并写入配置项

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+export default {
+    input: "src/main.js",
+    output: [
+        {
+            file: "./dist/ansheng-lib-umd.js",
+            format: "umd",
+            name: "anshengLib",
+            //当入口文件有export时，'umd'格式必须指定name
+            //这样，在通过<script>标签引入时，才能通过name访问到export的内容。
+        },{
+            file:"./dist/ansheng-lib-es.js",
+            format:"es"
+        },{
+            file:"./dist/ansheng-lib-cjs.js",
+            format:"cjs"
+        }
+    ]
+}
